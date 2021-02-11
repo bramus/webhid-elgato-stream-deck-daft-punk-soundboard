be1155704c9c96352c6fc8ecb29250c944a9e4be
Bramus <bramus@bram.us>
2021-02-11 22:12:37 +0100

    ✨ NPM Init + Add Snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,12 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    "src": "/"
+  },
+  devOptions: {
+    secure: true,
+  },
+};
