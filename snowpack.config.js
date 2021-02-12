304ab71f0adcb51955e40ccef7f043c309d2f8c0
Bramus <bramus@bram.us>
2021-02-12 13:22:52 +0100

    🚑 Add Babel to play nice with browsers that don't support private fields/methods (looking at you Firefox, and Safari)

--- snowpack.config.js
+++ snowpack.config.js
@@ -9,4 +9,19 @@ module.exports = {
   devOptions: {
     secure: true,
   },
+  plugins: [
+    [
+      "@snowpack/plugin-babel",
+      {
+        "input": ['.js', '.mjs', '.jsx', '.ts', '.tsx'], // (optional) specify files for Babel to transform
+        transformOptions: {
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-private-methods",
+            "@babel/plugin-proposal-private-property-in-object",
+          ]
+        }
+      }
+    ]
+  ]
 };
